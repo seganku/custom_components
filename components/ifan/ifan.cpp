a5f612f3ca0507bd65d3d2e99db979289ba2e96e
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-24 07:07:35 -0400

    full revert

--- components/ifan/ifan.cpp
+++ components/ifan/ifan.cpp
@@ -27,15 +27,14 @@ void IFan::control(const fan::FanCall &call) {
     this->speed = *call.get_speed();
   if (call.get_direction().has_value())
     this->direction = *call.get_direction();
-
-  this->write_state_();
   this->publish_state();
 
+  this->write_state_();
 }
 void IFan::write_state_() {
   if (this->direction_ != nullptr)
     this->direction_->set_state(this->direction == fan::FanDirection::REVERSE);
-  int local_speed = this->speed;
+  int local_speed = static_cast<int>(this->speed);
   ESP_LOGD("IFAN", "Setting Fan Speed %i", local_speed);
   ESP_LOGD("IFAN", "State is %i", this->state);
 
@@ -118,6 +117,7 @@ void IFan::long_beep(int num){
     delay(500);
   }
 }
+
 }  // namespace ifan
 
 }  // namespace esphome
--- components/ifan/ifan.h
+++ components/ifan/ifan.h
@@ -4,7 +4,7 @@
 #include "esphome/components/output/binary_output.h"
 #include "esphome/components/output/float_output.h"
 #include "esphome/components/fan/fan.h"
-
+#include "remote.h"
 namespace esphome {
 namespace ifan {
 
@@ -15,6 +15,7 @@ class IFan : public Component, public fan::Fan {
   void dump_config() override;
   void set_direction(output::BinaryOutput *direction) { this->direction_ = direction; }
   fan::FanTraits get_traits() override;
+
  protected:
   void control(const fan::FanCall &call) override;
   void write_state_();
@@ -26,11 +27,8 @@ class IFan : public Component, public fan::Fan {
   void beep(int num=1);
   void long_beep(int num=1);
 
-
   output::BinaryOutput *direction_{nullptr};
   int speed_count_{};
-  // for remote
-
 };
 
 }  // namespace speed
