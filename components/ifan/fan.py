836cdeb09bc1c65e9c5943f58e13a590b2517218
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-24 05:53:59 -0400

    add enable remote boolean

--- components/ifan/fan.py
+++ components/ifan/fan.py
@@ -4,6 +4,7 @@ from esphome.components import fan, output
 from esphome.const import (
     CONF_DIRECTION_OUTPUT,
     CONF_OUTPUT_ID,
+    CONF_ENABLE_REMOTE,
 )
 
 ifan_ns = cg.esphome_ns.namespace('ifan')
@@ -14,7 +15,7 @@ CONFIG_SCHEMA = fan.FAN_SCHEMA.extend(
     {
         cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(IFan),
         cv.Optional(CONF_DIRECTION_OUTPUT): cv.use_id(output.BinaryOutput),
-
+        cv.Required(CONF_ENABLE_REMOTE, default=False): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -23,7 +24,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await cg.register_component(var, config)
     await fan.register_fan(var, config)
-
     if CONF_DIRECTION_OUTPUT in config:
         direction_output = await cg.get_variable(config[CONF_DIRECTION_OUTPUT])
         cg.add(var.set_direction(direction_output))
+        cg.add(var.enable_remote(config[CONF_ENABLE_REMOTE]))
