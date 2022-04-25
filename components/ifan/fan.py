8d27065e940661a2cf6906851e8b5813168571c7
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-25 16:59:33 -0400

    add python components for remote

--- components/ifan/fan.py
+++ components/ifan/fan.py
@@ -1,15 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import fan
+from esphome.components import fan,uart
 from esphome.const import (
     CONF_OUTPUT_ID,
 )
+DEPENDENCIES = ['uart']
 
 BUZZER_ENABLE = "buzzer_enable"
 REMOTE_ENABLE = "remote_enable"
 
 ifan_ns = cg.esphome_ns.namespace("ifan")
 IFan = ifan_ns.class_("IFan", cg.Component, fan.Fan)
+IFan04 = ifan_ns.class_('IFan04', cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = fan.FAN_SCHEMA.extend(
     {
@@ -23,7 +25,10 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     cg.add(var.set_buzzer_enable(config[BUZZER_ENABLE]))
     cg.add(var.set_remote_enable(config[REMOTE_ENABLE]))
-
+    if REMOTE_ENABLE in config:
+        CONFIG_SCHEMA.extend(uart.UART_DEVICE_SCHEMA)
+        await cg.register_component(var, config)
+        await uart.register_uart_device(var, config)
     await cg.register_component(var, config)
 
     await fan.register_fan(var, config)
