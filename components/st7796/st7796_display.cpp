c5366e3e24ac805b1f6079d5eb9ea15509651853
Christopher P. Yarger <cpyarger@gmail.com>
2022-06-18 05:13:14 -0400

    asd

--- components/st7796/st7796_display.cpp
+++ components/st7796/st7796_display.cpp
@@ -276,7 +276,7 @@ void ST7796WT32::initialize() {
   this->init_lcd_(INITCMD_TFT);
   this->width_ = 320;
   this->height_ = 240;
-  this->fill_internal_(Color::red);
+  this->fill_internal_(Color::random_color());
 }
 
 }  // namespace st7796
