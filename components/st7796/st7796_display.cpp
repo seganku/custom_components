b2df7b15fa9eefd110a83771c555e88125cab9cb
Christopher P. Yarger <cpyarger@gmail.com>
2022-06-18 05:11:36 -0400

    test color

--- components/st7796/st7796_display.cpp
+++ components/st7796/st7796_display.cpp
@@ -276,7 +276,7 @@ void ST7796WT32::initialize() {
   this->init_lcd_(INITCMD_TFT);
   this->width_ = 320;
   this->height_ = 240;
-  this->fill_internal_(Color::RED);
+  this->fill_internal_(Color::red);
 }
 
 }  // namespace st7796
