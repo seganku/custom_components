822f77540a81e1b1a51b93b90318ca849f621ddb
Christopher P. Yarger <cpyarger@gmail.com>
2022-06-18 10:19:35 -0400

    sdf

--- components/st7796/st7796_display.cpp
+++ components/st7796/st7796_display.cpp
@@ -274,8 +274,8 @@ void ST7796TFT24::initialize() {
 
 void ST7796WT32::initialize() {
   this->init_lcd_(INITCMD_TFT);
-  this->width_ = 360;
-  this->height_ = 1000;
+  this->width_ = 320;
+  this->height_ = 480;
   //this->fill_internal_(Color::BLACK);
   //this->fill_internal_(ST7796colors::RED);
   //this->fill_internal_(ST7796colors::GREEN);
